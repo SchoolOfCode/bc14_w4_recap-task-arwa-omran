e521b4baf51e62d180d0d4eb7bdbfc1ddbbe6ab9
Arwa <124366130+arwa-omran@users.noreply.github.com>
2023-04-09 17:02:20 +0100

    changed tweet

--- tests/tester.test.js
+++ tests/tester.test.js
@@ -13,10 +13,10 @@ test('user can send a tweet', async ({ page }) => {
   await page.goto('http://localhost:3000');
   await page.fill('input[name="tweeter"]','Arwa');
   await expect(page.locator('input[name="tweeter"]')).toHaveValue('Arwa');
-  await page.fill('textarea[name="tweet"]', 'Hey SuperTester, do you even UI test, bro? Just kidding, we all know API testing is where the real fun is at. ;)');
-  await expect(page.locator('textarea[name="tweet"]')).toHaveValue('Hey SuperTester, do you even UI test, bro? Just kidding, we all know API testing is where the real fun is at. ;)');
+  await page.fill('textarea[name="tweet"]', "Back-end API testing? More like back-end API-tizing! You're making me hungry for some delicious APIs over here.");
+  await expect(page.locator('textarea[name="tweet"]')).toHaveValue("Back-end API testing? More like back-end API-tizing! You're making me hungry for some delicious APIs over here.");
   await page.click('button:has-text("Send tweet!")');
   await expect(page.locator('input[name="tweeter"]')).toHaveValue('');
   await expect(page.locator('textarea[name="tweet"]')).toHaveValue('');
-  await expect(page.locator('#tweets-list li:nth-child(4)')).toHaveText('Arwa Hey SuperTester, do you even UI test, bro? Just kidding, we all know API testing is where the real fun is at. ;)');
+  await expect(page.locator('#tweets-list li:nth-child(4)')).toHaveText("Arwa Back-end API testing? More like back-end API-tizing! You're making me hungry for some delicious APIs over here.");
 });
